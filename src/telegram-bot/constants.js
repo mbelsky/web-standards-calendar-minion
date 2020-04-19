9df6e74cd83d581113b1910e2a3057a485478a06
Max Belsky <public.belsky@gmail.com>
2020-04-19 22:14:15 +0300

    Обновляет константы сообщений

--- src/telegram-bot/constants.js
+++ src/telegram-bot/constants.js
@@ -1,9 +1,15 @@
 const BREAK_EVENT_CREATION_MESSAGE = 'Процесс создания события был прерван'
-const HELP_MESSAGE = 'Answer to /help'
+const HELP_MESSAGE = `
+/new – инициирует диалог создания события
+/cancel – прерывает диалог создания события
+`.trim()
+
 const OOPS_MESSAGE =
   'Ой, что-то пошло не так. ' +
   'Мы постараемся исправить проблему как можно скорее.'
-const START_MESSAGE = 'Answer to /start'
+const START_MESSAGE =
+  'Привет! Я помогаю добавлять события в календарь Веб-стандартов. ' +
+  'Чтобы добавить своё событие, отправь /new'
 const UNKNOWN_MESSAGE =
   'Извините, я не знаю как это обработать. ' +
   'Пожалуйста, используйте команды описанные в /help'
